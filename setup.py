91abe2ea5572f1a678ab576935146b92ccf1fd62
Riccardo Orlando <orlandoricc@gmail.com>
2020-07-18 11:08:13 +0200

    Upload files

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="srl-bert-verbatlas-riccorl",  # Replace with your own username
+    version="0.9",
+    author="Riccardo Orlando",
+    author_email="orlandoricc@gmail.com",
+    description="SRL Bert model trained with VerbAtlas inventory",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Riccorl/srl-bert-verbatlas",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "allennlp==0.9"
+    ],
+    python_requires=">=3.6",
+)
