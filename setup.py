7a8910e872068098d0bda53c9ee160bc654a216c
Riccardo Orlando <orlandoricc@gmail.com>
2020-07-19 03:14:43 +0200

    github action workflow

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="srl-bert-verbatlas",  # Replace with your own username
-    version="0.9.3.2",
+    version="0.9.4",
     author="Riccardo Orlando",
     author_email="orlandoricc@gmail.com",
     description="SRL Bert model trained with VerbAtlas inventory",
