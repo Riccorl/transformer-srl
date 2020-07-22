7694c782725a552c263b687526f5f6349b16e788
Riccardo Orlando <orlandoricc@gmail.com>
2020-07-22 13:18:40 +0200

    update lemma2va

--- scripts/bert_base_un.sh
+++ scripts/bert_base_un.sh
@@ -3,12 +3,14 @@ source /home/orlando/miniconda3/bin/activate allennlp-09
 
 HOME="/home/orlando"
 DATASET="$HOME/datasets/ontonotes/conll-formatted-ontonotes-verbatlas"
-PROJECT="$HOME/srl-bert-span"
+PROJECT="$HOME/srl-bert-verbatlas"
 
 export SRL_TRAIN_DATA_PATH="$HOME/datasets/conll_like/un/data/train"
 export SRL_VALIDATION_DATA_PATH="$DATASET/data/development"
 
 CONFIG="$PROJECT/training_config/srl_bert_base.jsonnet"
+
+free_mem=$(nvidia-smi --query-gpu=memory.free --format=csv -i 1 | grep -Eo [0-9]+)
 
 echo "$free_mem MB"
 while [ "$free_mem" -lt 10000 ]; do
