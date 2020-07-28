a54ac6ab23984aa9c2be4d98e1e8f58679077cdd
Riccardo Orlando <orlandoricc@gmail.com>
2020-07-28 17:18:52 +0200

    fix data reader

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="transformer_srl",  # Replace with your own username
-    version="2.0.dev20202807b2",
+    version="2.1.dev20202807",
     author="Riccardo Orlando",
     author_email="orlandoricc@gmail.com",
     description="SRL Transformer model",
