e721e6b9b9f8593c164d141cfe7410c4135643ef
Riccardo Orlando <orlandoricc@gmail.com>
2020-08-14 11:41:04 +0200

    update version number, update allennlp requirements

--- scripts/train_test.sh
+++ scripts/train_test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-source /Users/ric/miniconda3/bin/activate srl-mt
+source /home/pollo/miniconda3/bin/activate srl-mt
 
 # local
-DATASET="data/semeval/"
-PROJECT="/Users/ric/Documents/ComputerScience/Projects/transformer-srl"
+DATASET="/mnt/d/Datasets/conll2012/conll-formatted-ontonotes-5.0-subset"
+PROJECT="/mnt/c/Users/rikkw/Desktop/Ric/Projects/transformer-srl"
 
-export SRL_TRAIN_DATA_PATH="$DATASET"
-export SRL_VALIDATION_DATA_PATH="$DATASET"
+export SRL_TRAIN_DATA_PATH="$DATASET/data/train"
+export SRL_VALIDATION_DATA_PATH="$DATASET/data/development"
 
 CONFIG="$PROJECT/training_config/bert_tiny_span.jsonnet"
 
